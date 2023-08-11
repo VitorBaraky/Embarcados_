435762014ced39f74a9edc23965ffc3b8945e92c
victorhug97 <victorhug97@gmail.com>
2023-08-11 12:01:27 -0300

    Add initial folders (project and exercises)

---
+++ Project/main/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_log.h"
+
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    uint32_t flash_size;
+    esp_chip_info(&chip_info);
+    static const char* TAG = "MyModule";
+    esp_log_level_set("MyModule", ESP_LOG_WARN);
+    
+
+    ESP_LOGI(TAG, "This is %s chip model %u with %d CPU core(s) WiFi%s%s%s, ", 
+                                                                CONFIG_IDF_TARGET, chip_info.model, chip_info.cores,
+                                                                (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+                                                                (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "",
+                                                                (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "" );
+
+    unsigned major_rev = chip_info.revision / 100;
+    unsigned minor_rev = chip_info.revision % 100;
+    ESP_LOGI(TAG, "silicon revision v%d.%d, ", major_rev, minor_rev);
+    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed");
+        return;
+    }
+
+    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+
+    int i = 0;
+
+    while(1)
+    {
+        i++;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGW(TAG,"The program is running %d ...", i);
+    }
+
+    /*for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }*/
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
