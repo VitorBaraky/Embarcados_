d829321919cd1e0de305e6d2e5290226c6949bf4
Vitor Baraky Fagundes <vitor.baraky@gmail.com>
2023-08-31 21:09:55 -0300

    Update main.c

--- Project/main/main.c
+++ Project/main/main.c
@@ -7,12 +7,74 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_log.h"
+#include "driver/gpio.h"
+#include "freertos/queue.h"
 // Global TAGs
  static const char* TAG = "MyModule";
+
+#define GPIO_INPUT_IO_0    21
+#define GPIO_INPUT_IO_1    22
+#define GPIO_INPUT_IO_2    23
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1)| (1ULL<<GPIO_INPUT_IO_2))
+
+
+#define GPIO_OUTPUT_IO_0 2 
+#define GPIO_OUTPUT_PIN_SEL 1ULL<<GPIO_INPUT_IO_0
+
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static QueueHandle_t gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
+
+
 // Main task
 void app_main(void)
 {
-    printf("Hello world!\n");
+    //Input Config
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf); //configurou os IOs específicos, agora pode configurar outros
+
+
+    //Output Config
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t)); //criação de fila
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL); //task do gpio
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
+    gpio_isr_handler_add(GPIO_INPUT_IO_2, gpio_isr_handler, (void*) GPIO_INPUT_IO_2);
+
+
+
+
     //Set variables
     esp_chip_info_t chip_info;
     uint32_t flash_size;
@@ -29,7 +91,7 @@ void app_main(void)
     unsigned minor_rev = chip_info.revision % 100;
     ESP_LOGI(TAG, "silicon revision v%d.%d, ", major_rev, minor_rev);
     if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
+        ESP_LOGE(TAG, "Get flash size failed");
         return;
     }
     printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
@@ -42,13 +104,10 @@ void app_main(void)
     {
         i++;
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        ESP_LOGW(TAG,"The program is running %d ...", i);
+        ESP_LOGI(TAG,"The program is running %d ...", i);
     }
-    /*for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }*/
-    printf("Restarting now.\n");
+   
+    /*printf("Restarting now.\n");
     fflush(stdout);
-    esp_restart();
+    esp_restart();*/
 }
