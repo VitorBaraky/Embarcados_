b3171450bcfeccbf2a240de4c07c8b99fb4c05d5
Victor Hugo <victorhug97@gmail.com>
2023-09-01 11:27:04 -0300

    Código_Prática2

--- Project/main/main.c
+++ Project/main/main.c
@@ -19,9 +19,11 @@
 
 
 #define GPIO_OUTPUT_IO_0 2 
-#define GPIO_OUTPUT_PIN_SEL 1ULL<<GPIO_INPUT_IO_0
+#define GPIO_OUTPUT_PIN_SEL 1ULL<<GPIO_OUTPUT_IO_0
 
 #define ESP_INTR_FLAG_DEFAULT 0
+
+uint32_t led_state = 0;
 
 static QueueHandle_t gpio_evt_queue = NULL;
 
@@ -29,18 +31,41 @@ static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     uint32_t gpio_num = (uint32_t) arg;
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+    
 }
 
-static void gpio_task_example(void* arg)
+static void gpio_task(void* arg)
 {
     uint32_t io_num;
     for(;;) {
         if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
             printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            switch (io_num)
+            {
+                case GPIO_INPUT_IO_0:
+                    gpio_set_level(GPIO_OUTPUT_IO_0,1);
+                    led_state = 1;
+                break;
+                case GPIO_INPUT_IO_1:
+                    gpio_set_level(GPIO_OUTPUT_IO_0,0);
+                    led_state = 0; 
+                break;
+                case GPIO_INPUT_IO_2:
+                    if (led_state){
+                        gpio_set_level(GPIO_OUTPUT_IO_0,0);
+                        led_state = 0;
+                    }
+                    else{
+                        gpio_set_level(GPIO_OUTPUT_IO_0,1);
+                        led_state = 1;
+                    };
+                break;
+                default:
+                break;
+            }
         }
     }
 }
-
 
 // Main task
 void app_main(void)
@@ -51,7 +76,7 @@ void app_main(void)
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = 1;
-    gpio_config(&io_conf); //configurou os IOs específicos, agora pode configurar outros
+    gpio_config(&io_conf); //configuração das entradas específicas
 
 
     //Output Config
@@ -60,20 +85,18 @@ void app_main(void)
     io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
     io_conf.pull_down_en = 0;
     io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
+    gpio_config(&io_conf); // configuração das saídas
 
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t)); //criação de fila
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL); //task do gpio
+    xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL); //task do gpio
 
     //install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     //hook isr handler for specific gpio pin
+
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
     gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
     gpio_isr_handler_add(GPIO_INPUT_IO_2, gpio_isr_handler, (void*) GPIO_INPUT_IO_2);
-
-
-
 
     //Set variables
     esp_chip_info_t chip_info;
