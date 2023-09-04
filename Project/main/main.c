913e41888bf465b8231d026dcc99beddef14963a
Vitor Baraky Fagundes <vitor.baraky@gmail.com>
2023-09-04 10:50:08 -0300

    Add comments - Vitor Baraky

--- Project/main/main.c
+++ Project/main/main.c
@@ -1,3 +1,5 @@
+// Project - Embedded systems - ESP32
+// Group : Vitor Baraky and Victor Hugo
 // Library
 #include <stdio.h>
 #include <inttypes.h>
@@ -9,39 +11,36 @@
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "freertos/queue.h"
-// Global TAGs
- static const char* TAG = "MyModule";
-
+// Defining Global TAGs
+ static const char* TAG = "ESP_32";
+// Defining global variables
+// Inputs
 #define GPIO_INPUT_IO_0    21
 #define GPIO_INPUT_IO_1    22
 #define GPIO_INPUT_IO_2    23
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1)| (1ULL<<GPIO_INPUT_IO_2))
-
-
+// Outputs
 #define GPIO_OUTPUT_IO_0 2 
 #define GPIO_OUTPUT_PIN_SEL 1ULL<<GPIO_OUTPUT_IO_0
-
 #define ESP_INTR_FLAG_DEFAULT 0
-
-uint32_t led_state = 0; //variável responsável por monitorar o estado do led
-
+// LED STATE variable
+uint32_t led_state = 0; // variable responsable to register the LED state
+// define event queue
 static QueueHandle_t gpio_evt_queue = NULL;
-
-//task da interrupção
+// Interruption tasks
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
-    
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);  
 }
-//task do GPIO
+// GPIO task
 static void gpio_task(void* arg)
 {
-    uint32_t io_num;
+    uint32_t io_num; // IO number received by the Queue
     for(;;) {
         if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
-            //Lógica do acionamento do led
+            ESP_LOGI(TAG,"GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            // LED activation logic
             switch (io_num)
             {
                 case GPIO_INPUT_IO_0:
@@ -53,7 +52,7 @@ static void gpio_task(void* arg)
                     led_state = 0; 
                 break;
                 case GPIO_INPUT_IO_2:
-                    //Avalia o estado do led através do led_state
+                    // Evaluate LED state
                     if (led_state){
                         gpio_set_level(GPIO_OUTPUT_IO_0,0);
                         led_state = 0;
@@ -69,43 +68,37 @@ static void gpio_task(void* arg)
         }
     }
 }
-
 // Main task
 void app_main(void)
 {
-    //Input Config
+    // Input Config
     gpio_config_t io_conf = {};
     io_conf.intr_type = GPIO_INTR_NEGEDGE;
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = 1;
-    gpio_config(&io_conf); //configuração das entradas específicas
-
-
-    //Output Config
+    gpio_config(&io_conf); // Configuring specific inputs 
+    // Output Config
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
     io_conf.pull_down_en = 0;
     io_conf.pull_up_en = 0;
-    gpio_config(&io_conf); // configuração das saídas
-
+    gpio_config(&io_conf); // Configuring specific outputs
+    // Creating queue
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t)); //criação de fila
     xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL); //task do gpio
-
-    //install gpio isr service
+    // install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    //hook isr handler for specific gpio pin
-
+    // hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
     gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
     gpio_isr_handler_add(GPIO_INPUT_IO_2, gpio_isr_handler, (void*) GPIO_INPUT_IO_2);
-
-    //Set variables
+    // Set variables
     esp_chip_info_t chip_info;
     uint32_t flash_size;
     esp_chip_info(&chip_info);
-    esp_log_level_set("MyModule", ESP_LOG_INFO);
+    esp_log_level_set("ESP_32", ESP_LOG_INFO);
     // Print LOG
     ESP_LOGI(TAG, "This is %s chip model %u with %d CPU core(s) WiFi%s%s%s, ", 
                                                                 CONFIG_IDF_TARGET, chip_info.model, chip_info.cores,
@@ -120,10 +113,10 @@ void app_main(void)
         ESP_LOGE(TAG, "Get flash size failed");
         return;
     }
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+    ESP_LOGI(TAG,"%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+    ESP_LOGI(TAG,"Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
     // Block loop
     int i = 0;
     while(1)
