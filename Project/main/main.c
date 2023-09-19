1166153158684c143c1684b0ad767ed5cac1833b
Vitor Baraky Fagundes <vitor.baraky@gmail.com>
2023-09-18 23:09:41 -0300

    Update main.c

--- Project/main/main.c
+++ Project/main/main.c
@@ -11,8 +11,11 @@
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "freertos/queue.h"
+#include "driver/gptimer.h"
 // Defining Global TAGs
  static const char* TAG = "ESP_32";
+ static const char* TAG_GPIO = "GPIO";
+ static const char* TAG_TIMER = "TIMER";
 // Defining global variables
 // Inputs
 #define GPIO_INPUT_IO_0    21
@@ -25,9 +28,11 @@
 #define ESP_INTR_FLAG_DEFAULT 0
 // LED STATE variable
 uint32_t led_state = 0; // variable responsable to register the LED state
+
+//////////////////////////////////////////// - GPIO - //////////////////////////////////////////////////
 // define event queue
 static QueueHandle_t gpio_evt_queue = NULL;
-// Interruption tasks
+// GPIO Interruption tasks
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     uint32_t gpio_num = (uint32_t) arg;
@@ -39,7 +44,7 @@ static void gpio_task(void* arg)
     uint32_t io_num; // IO number received by the Queue
     for(;;) {
         if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            ESP_LOGI(TAG,"GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            ESP_LOGI(TAG_GPIO,"GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
             // LED activation logic
             switch (io_num)
             {
@@ -68,32 +73,44 @@ static void gpio_task(void* arg)
         }
     }
 }
-// Main task
+
+//////////////////////////////////////////// - TIMER - //////////////////////////////////////////////////
+// Defining event count struct
+typedef struct {
+    uint64_t event_count;
+} queue_element_t;
+// Starting Timer Queue
+static QueueHandle_t Timer_evt_queue = NULL;
+// Timer Interruption task
+static bool IRAM_ATTR alarm_v1(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data){
+    BaseType_t high_task_awoken = pdFALSE;
+    // Retrieve count value and send to queue
+    queue_element_t timer_element = {
+        .event_count = edata->count_value
+    };
+    xQueueSendFromISR(Timer_evt_queue, &timer_element, NULL);
+    gptimer_alarm_config_t alarm_config = {
+        .alarm_count = edata->alarm_value + 100000, // alarm in next 100ms
+    };
+    return (high_task_awoken == pdTRUE);
+}
+// Time task
+static void task_Timer(void* arg){
+    queue_element_t timer_element;
+    for(;;){
+     if (xQueueReceive(Timer_evt_queue, &timer_element, portMAX_DELAY)) {
+          ESP_LOGI(TAG_TIMER, "Timer stopped, count=%llu", timer_element.event_count);
+        } 
+     else {
+        ESP_LOGW(TAG_TIMER, "Missed one count event");
+        }
+    }
+}
+
+//////////////////////////////////////////// - Main Task - //////////////////////////////////////////////////// 
 void app_main(void)
 {
-    // Input Config
-    gpio_config_t io_conf = {};
-    io_conf.intr_type = GPIO_INTR_NEGEDGE;
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf); // Configuring specific inputs 
-    // Output Config
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
-    gpio_config(&io_conf); // Configuring specific outputs
-    // Creating queue
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t)); //criação de fila
-    xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL); //task do gpio
-    // install gpio isr service
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    // hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
-    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
-    gpio_isr_handler_add(GPIO_INPUT_IO_2, gpio_isr_handler, (void*) GPIO_INPUT_IO_2);
+    // ---------------------------------------- Prática 1 Chip info -------------------------------------------- //
     // Set variables
     esp_chip_info_t chip_info;
     uint32_t flash_size;
@@ -117,13 +134,67 @@ void app_main(void)
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
     ESP_LOGI(TAG,"Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-    // Block loop
+
+    // ---------------------------------------- Prática 2 GPIO -------------------------------------------- // 
+    // Input Config
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf); // Configuring specific inputs 
+    // Output Config
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf); // Configuring specific outputs
+    // Creating queue
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t)); //criação de fila
+    xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL); //task do gpio
+    // install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
+    gpio_isr_handler_add(GPIO_INPUT_IO_2, gpio_isr_handler, (void*) GPIO_INPUT_IO_2);
+
+    // ---------------------------------------- Prática 3 Timer  -------------------------------------------- // 
+    // Creating TIMER Queue and task 
+    xTaskCreate(task_Timer, "Timer_task", 2048, NULL, 10, NULL); 
+    Timer_evt_queue = xQueueCreate(10, sizeof(queue_element_t));
+    if (!Timer_evt_queue) {        
+        ESP_LOGE(TAG_TIMER, "Creating queue failed");
+        return;
+    }
+    ESP_LOGI(TAG_TIMER, "Create timer handle");
+    gptimer_handle_t gptimer = NULL;
+    gptimer_config_t timer_config = {
+        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+        .direction = GPTIMER_COUNT_UP,
+        .resolution_hz = 1000000, // 1MHz, 1 tick=1us
+    };
+    ESP_ERROR_CHECK(gptimer_new_timer(&timer_config, &gptimer));
+
+    gptimer_event_callbacks_t call_Back = {
+        .on_alarm = alarm_v1,
+    };
+    ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer, &call_Back, NULL));
+    ESP_ERROR_CHECK(gptimer_enable(gptimer));
+    ESP_LOGI(TAG, "Start timer, update alarm value dynamically");
+    gptimer_alarm_config_t alarm_config1 = {
+        .alarm_count = 100000, // period = 100ms
+    };
+    ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer, &alarm_config1));
+    ESP_ERROR_CHECK(gptimer_start(gptimer));
+
+    // ---------------------------------------- Block loop  -------------------------------------------- // // 
     int i = 0;
     while(1)
     {
-        i++;
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        ESP_LOGI(TAG,"The program is running %d ...", i);
+       i++;
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+       ESP_LOGI(TAG,"The program is running %d ...", i);
     }
-
 }
